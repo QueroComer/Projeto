7c346db43ddc545a9def490eaa4e0c2c200694d9
amkarina <am.karina@hotmail.com>
2015-09-13 19:33:48 -0300

    Login com o Facebook

--- QueroComer/QueroComer/Scripts/Facebook.js
+++ QueroComer/QueroComer/Scripts/Facebook.js
@@ -8,7 +8,7 @@ function statusChangeCallback(response) {
     // for FB.getLoginStatus().
     if (response.status === 'connected') {
         // Logged into your app and Facebook.
-        testAPI();
+        loginComFacebook();
     } else if (response.status === 'not_authorized') {
         // The person is logged into Facebook, but not your app.
         document.getElementById('status').innerHTML = 'Please log ' +
@@ -68,9 +68,17 @@ window.fbAsyncInit = function () {
 
 // Here we run a very simple test of the Graph API after login is
 // successful.  See statusChangeCallback() for when this call is made.
-function testAPI() {    
-    FB.api('/me', function (response) {
-        alert("Bem-vindo(a) " + response.name);
-        console.log(JSON.stringify(response));        
-    });
+function loginComFacebook() {
+
+    FB.login(function (response) {
+        if (response.authResponse) {
+            console.log('Welcome!  Fetching your information.... ');
+            FB.api('/me', function (response) {
+                console.log('Good to see you, ' + response.name + '.');
+                console.log(JSON.stringify(response));
+            });
+        } else {
+            console.log('User cancelled login or did not fully authorize.');
+        }
+    }, { scope: 'email,user_likes' , return_scopes: true});
 }
